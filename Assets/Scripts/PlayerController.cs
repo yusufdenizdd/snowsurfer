e3d2ae1456617049bfd9709a71dedc6d52bb31d8
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2025-10-26 20:33:12 +0300

    input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    InputAction moveAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+        print(moveVector);
 
     }
 }
