e207628e40bef752c2f0ae6f96a98e0d13f72a1f
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2025-11-18 22:19:56 +0300

    reloadscene

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float invokeTime = 1f;
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if (other.gameObject.layer == layerIndex)
         {
             print("kafanı yere vurduni kaybettin");
+            //SceneManager.LoadScene(0);
+            Invoke("ReloadScene", invokeTime);
         }
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float invokeTime = 1f;
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
 
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         // if (other.CompareTag("Player"))
         // {
         //     print("bitti");
         // }
+
         int layerIndex = LayerMask.NameToLayer("Player"); // 6
         if (other.gameObject.layer == layerIndex)
         {
             print("çizgiyi geçtin, kazandın");
+            //SceneManager.LoadScene(0);
+            Invoke("ReloadScene", invokeTime);
         }
+
     }
+
 
 
 }
